f92775e475ab9a555c22bdc27c409063253e5c9c
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-09-14 12:53:39 +0600

    loged in and not loged in user condition added

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,12 +1,17 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
 
-for(var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function (){
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action', action)
 
         console.log('USER:', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not logged in')
+        } else {
+            console.log('user is loged in, sending data...')
+        }
     })
 }
