088f8d2c14343da1d0b71db8c4c14be471730757
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-09-15 15:32:58 +0600

    action added to view

--- store/views.py
+++ store/views.py
@@ -38,6 +38,8 @@ def checkout(request):
 def updateItem(request):
     data = json.loads(request.data)
     productId = data['productId']
+    action = data['action']
 
     print('productId:', productId)
+    print('action:', action)
     return JsonResponse('Item was added', safe=False)
