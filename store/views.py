e00ac8439014685cc2cbe2274eb786c333d93e8f
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-09-18 15:08:21 +0600

    cart item added to cart page

--- store/views.py
+++ store/views.py
@@ -28,10 +28,13 @@ def cart(request):
         customer = request.user.customer
         order, create = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0 }
-    context = {'items': items, 'order': order}
+        cartItems = order.get_cart_items
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
